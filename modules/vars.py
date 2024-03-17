d9648b97aff63e858ff59aa2ed3d55f1289d9570
om705 <61368509+om705@users.noreply.github.com>
2024-03-17 07:23:23 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-eapi_id = "23421759"
-api_hash = "ba8ae5b1f703fc771d1e8425db8752d1"
-bot_token = "7092334965:AAErP4tkv5Cy8fRnTeC03_sdmPNGueNmjUg"
+eapi_id = "22114233"
+api_hash = ""
+bot_token = "7106149439:AAGlV9V_HcPIhMDLrMfPN-itr0gd_xLb0Aw"
